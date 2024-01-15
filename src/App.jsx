232fa29ed857334161f65db4b5b292b1e9e5c660
Hardik <test@test.com>
2024-01-15 14:24:19 +0530

    Addition: Dark and light theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
 import Navbar from './components/navbar'
 import Content from './components/content'
+import React from 'react'
 
 function App() {
+  const [mode, setMode] = React.useState('dark')
+
+  function handleClick(){
+    setMode(prevMode => {
+      const newMode = prevMode == 'dark' ? 'light' : 'dark'
+      return newMode
+    })
+    console.log('Mode Changed')
+  }
+
+  console.log("Mode", mode)
+
+
   return (
-    <div>
-      <Navbar />
-      <Content />
+    <div className={mode == 'dark' ? '' : 'app'}>
+      <Navbar mode={mode} modeChange = {handleClick} />
+      <Content mode={mode} />
     </div>  
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,24 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from "react"
 import IMAGES from "../images/all-images"
 
-export default function Navbar(){
+export default function Navbar(props){
     return (
-        <nav className='header-bar'>
-            <div className="react-logo">
-                <img src={IMAGES.image1} alt='React Image' className='header-img' />
-                <h1>ReactFacts</h1>
-            </div>
-            <p>React Course - Project 1</p>
-        </nav>
+        <div className={props.mode == 'dark' ? '' : 'lightN'}>
+            <nav className="header-bar">
+                <div className="react-logo">
+                    <img src={IMAGES.image1} alt='React Image' className='header-img' />
+                    <h1>ReactFacts</h1>
+                </div>
+                <section className="mode">
+                    <p className="mode-light">Light</p>
+                    <span className="mode-outer-circle" onClick={props.modeChange}>
+                        <span className="mode-inner-circle"></span>
+                    </span>
+                    <p className="mode-dark">Dark</p>
+                </section>
+            </nav>
+        </div>
     )
 }
